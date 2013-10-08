cd1e6839f2dacf6cd8b6dab4d1867a75553fc884
Marco Prayer iMac Sentia <marco.prayer@sentia.com.au>
2013-10-08 16:46:29 +1100

    changed the images

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  
+  #TDD automatic tester
+  RSpec.configure do |config|
+    config.include Capybara::DSL
+  end
 end
